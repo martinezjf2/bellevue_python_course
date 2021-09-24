ccedfec162006039caad72d7abc7110c1c57b05b
Jeffrey Martinez <martinezjf2@gmail.com>
2021-09-23 21:59:00 -0400

    fixed my conditional statements

--- assignment_4.py
+++ assignment_4.py
@@ -15,11 +15,11 @@ number_in_feet_fiber_optic = float(input("\nEnter the number of feet of fiber op
 
 # Modify your week 3 program so that if the user purchases more than 100 feet they are charged $.80 per foot.  If the user purchases more than 250 feet they will be charged $.70 per foot.  If they purchase more than 500 feet they will be charged $.50 per foot.
 
-if number_in_feet_fiber_optic >= 100 and number_in_feet_fiber_optic < 250:
+if number_in_feet_fiber_optic > 100 and number_in_feet_fiber_optic <= 250:
     cost_per_feet = 0.80
-elif number_in_feet_fiber_optic >= 250 and number_in_feet_fiber_optic < 500:
+elif number_in_feet_fiber_optic > 250 and number_in_feet_fiber_optic <= 500:
     cost_per_feet = 0.70
-elif number_in_feet_fiber_optic >= 500:
+elif number_in_feet_fiber_optic > 500:
     cost_per_feet = .50
 else:
     cost_per_feet = .87
@@ -39,9 +39,9 @@ print(final_statement)
 
 # Conditionals using if/elif/else statements
 
-# 18-19: Check that if conditional was greater than or equal to 100 and if number_of_feet was less than 250, then declare and assign cost_per_feet to be .80
-# 20-21: Used elif if first conditional evaluates to false, checked number_of_feet was greater than or equal to 250 and if number_of_feet was less than 500, assign cost_per_feet to be .70
-# 22-23: Used elif if first and second conditions evaluates to false, checked number_of_feet was greater than or equal to 500, if it evaluates to true, assign cost_per_feet to be .50
+# 18-19: Check that if conditional was greater than 100 and if number_of_feet was less than or equal to 250, then declare and assign cost_per_feet to be .80
+# 20-21: Used elif if first conditional evaluates to false, checked number_of_feet was greater than 250 and if number_of_feet was less than or equal to 500, assign cost_per_feet to be .70
+# 22-23: Used elif if first and second conditions evaluates to false, checked number_of_feet was greater than 500, if it evaluates to true, assign cost_per_feet to be .50
 # 24-25: Used an else statement to assign cost_per_feet to be .87 if all conditions above evaluate to false.
 
 
