ef3d9ff4a48ece881f82f5eee644f680c80eaa01
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-02 17:48:51 -0400

    added option for units of measurement, passed down units all the way down to the display_weather, passed in units for the retreiving API call in a specific measurement

--- assignment_9.py
+++ assignment_9.py
@@ -11,6 +11,8 @@ KEY = os.getenv("KEY")
 
 def main():
     welcome()
+    # type_zip_or_city_name()
+    # delete the user_input() call on line 16
     user_input()
     print()
     
@@ -20,23 +22,28 @@ def welcome():
     cprint("-------------------------------------------------\n", 'blue')
 
 
-def get_weather(name):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={name}&appid={KEY}"
+def get_weather(name, units):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={name}&appid={KEY}&units={units}"
     request = requests.get(url)
     # https://www.youtube.com/watch?v=lcWfSn6-m_8 video on how to use the requests to retreive data
-    validate_request(request) 
+    validate_request(request, units) 
     
-def get_weather_by_zip(zip):
-    print()
     
 def user_input():
     city_name = input('Please type in the city: ')
     if (city_name == 'exit' or city_name == 'Exit' or city_name == 'quit' or city_name == 'Quit' or city_name == 'q' or city_name == 'Q'):
-        print("\nThank you! Hope you have a great day!\n") 
-    else:
-        get_weather(city_name)
+        print("\nThank you! Hope you have a great day!\n")
+    choose_units(city_name)    
+        
+        
+def choose_units(city_name):       
+    choose_unit = input("Type '1' for metric, Type '2' for standard, or Type '3' for imperial: ")
+    unit = check_units_of_measure(choose_unit)
+    get_weather(city_name, unit)
     
-def display_weather(name, main, description, temperature, temp_max, temp_min):
+    
+def display_weather(name, main, description, temperature, temp_max, temp_min, units):
+    print(units)
     print("\nCity Name: ", name)
     print("Main: ", main)
     print("Desciption: ", description)
@@ -45,23 +52,23 @@ def display_weather(name, main, description, temperature, temp_max, temp_min):
     print("Temp Min ", temp_min, '\n')
     closing_or_choose()
     
-def validate_request(request):
-    print(f"Status Code: {request.status_code}")
+def validate_request(request, units):
+    # print(f"Status Code: {request.status_code}")
     if (request.status_code == requests.codes.ok):
         promise = request.json()
-        retrieve_details(promise)
+        retrieve_details(promise, units)
     else:
         print("Please Provide with a Valid City Name: \n")
         user_input()
     
-def retrieve_details(promise):
+def retrieve_details(promise, units):
     main = promise['weather'][0]['main']
     description = promise['weather'][0]['description']
     temperature = promise["main"]["temp"]
     temp_min = promise["main"]["temp_min"]
     temp_max = promise["main"]["temp_max"]
     name = promise['name']
-    display_weather(name, main, description, temperature, temp_max, temp_min)
+    display_weather(name, main, description, temperature, temp_max, temp_min, units)
     
 def kelvin_to_fahrenheit(temp, temp_min, temp_max):
     print()
@@ -69,11 +76,26 @@ def kelvin_to_fahrenheit(temp, temp_min, temp_max):
 def kelvin_to_celsius(temp, temp_min, temp_max):
     print()
     
-def type_zip_or_city_name():
-    city_or_zip = input("Type '1' to input zip OR type '2' to input city name")
+# def type_zip_or_city_name():
+#     city_or_zip = input("Type '1' to input zip OR type '2' to input city name")
+#     if (city_or_zip == '1'):
+#         zipcode = input("Please enter a zipcode: ")
+#         get_weather_by_zip(zipcode)
+#     elif (city_or_zip == '2'):
+#         user_input()
     
-def choose_units_of_measure():
-    print()
+def check_units_of_measure(input):
+    print(input)
+    if (input == '1'):
+       return 'metric'
+    elif (input == '2'):
+       return 'standard'
+    elif (input == '3'):
+        return 'imperial'
+    else:
+        print("Please type in a valid number!")
+        choose_units()
+        
 
     
 def closing_or_choose():
