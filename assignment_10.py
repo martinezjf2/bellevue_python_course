66a41f3574c8f29e71219d6e43dfb0550b2217ec
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-07 23:22:05 -0500

    made a new function to write in file

--- assignment_10.py
+++ assignment_10.py
@@ -3,17 +3,22 @@ import os
 
 
 def main():
-    print()
+    ask_user_directory()
+    
 
 def ask_user_filename():
     file_input = input("What is the name of the file you are trying to get? ")
-    filename = filename_exist(file_input)
-    return filename
+    return file_input
     
 def ask_user_directory():
     directory_input = input("What is the name of the directory you are trying to get access to?")
     directory = directory_exist(directory_input)
     filename = ask_user_filename()
+    write_in_file(directory, filename)
+    
+def write_in_file(dir, filename):
+    file = open(os.path.join(dir, filename), 'w')
+    
     
     
 def directory_exist(dir):
@@ -24,18 +29,12 @@ def directory_exist(dir):
         directory = os.mkdir(dir)
         print("Folder Created")
         return directory
-    
-def filename_exist(file):
-    tru = os.path.isfile(file)
-    if (tru):
-        fil = open(file, "r+")
-        return fil
-    else:
-        fil = open(file, "r+")
-        return fil
-      
+        
     
 def ask_personal_info():
     name = input("What is your name? ")
     address = input("What is your address? ")
     phone_number = input("what is your phone number? ")
+    
+    
+main()
