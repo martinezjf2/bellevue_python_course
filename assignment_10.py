78058c4e4735c1620539bc5a92c6b7b32c68599f
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-07 22:11:23 -0500

    added some notes and constructed functions to breakdown code

---
+++ assignment_10.py
@@ -0,0 +1,18 @@
+
+def main():
+    print()
+
+def ask_user_filename():
+    file = input("What is the name of the file you are trying to get? ")
+    
+def ask_user_directory():
+    directory = input("What is the name of the directory you are trying to get access to?")
+    
+def directory_exist():
+    print()
+    # need if/else statement to if found or create new directory
+    
+def ask_personal_info():
+    name = input("What is your name? ")
+    address = input("What is your address? ")
+    phone_number = input("what is your phone number? ")
