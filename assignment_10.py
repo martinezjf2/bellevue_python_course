05186add70584a7440a65da745cc3112cf329f5e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-07 22:35:35 -0500

    added filename and directory if/else statements

--- assignment_10.py
+++ assignment_10.py
@@ -9,14 +9,28 @@ def ask_user_filename():
     file = input("What is the name of the file you are trying to get? ")
     
 def ask_user_directory():
-    directory = input("What is the name of the directory you are trying to get access to?")
+    directory_input = input("What is the name of the directory you are trying to get access to?")
+    directory = directory_exist(directory_input)
+    
     
 def directory_exist(dir):
+    directory = ''
     tru = os.path.isdir(dir)
-    
+    if (tru):
+        directory = dir
+    else:
+        directory = os.mkdir(dir)
+        print("Folder Created")
+    return directory
     
 def filename_exist(file):
+    fil = ''
     tru = os.path.isfile(file)
+    if (tru):
+        fil = open(file, "r+")
+    else:
+        fil = open(file, "r+")
+    return fil
       
     
 def ask_personal_info():
