ff7db61fa76066934653b5f3db35919aafc02b50
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-07 22:21:10 -0500

    added the os methods to return true or false if file and directory exists

--- assignment_10.py
+++ assignment_10.py
@@ -1,4 +1,7 @@
 
+import os
+
+
 def main():
     print()
 
@@ -8,9 +11,13 @@ def ask_user_filename():
 def ask_user_directory():
     directory = input("What is the name of the directory you are trying to get access to?")
     
-def directory_exist():
-    print()
-    # need if/else statement to if found or create new directory
+def directory_exist(dir):
+    tru = os.path.isdir(dir)
+    
+    
+def filename_exist(file):
+    tru = os.path.isfile(file)
+      
     
 def ask_personal_info():
     name = input("What is your name? ")
