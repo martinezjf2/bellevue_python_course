d82994b6b127bbbef91abc632a79255c0560f711
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-07 23:37:06 -0500

    tested by adding new folders and new files, added new line breaks,

--- assignment_10.py
+++ assignment_10.py
@@ -7,29 +7,33 @@ def main():
     
 
 def ask_user_filename():
-    file_input = input("What is the name of the file you are trying to get? ")
+    file_input = input("\nWhat is the name of the file you are trying to get? ")
     return file_input
     
 def ask_user_directory():
-    directory_input = input("What is the name of the directory you are trying to get access to?")
+    directory_input = input("\nWhat is the name of the directory you are trying to get access to? ")
     directory = directory_exist(directory_input)
     filename = ask_user_filename()
     write_in_file(directory, filename)
     read_in_file(directory, filename)
     
 def write_in_file(dir, filename):
-    name = input("What is your name? ")
-    address = input("What is your address? ")
-    phone_number = input("what is your phone number? ")
+    name = input("\nWhat is your name? ")
+    address = input("\nWhat is your address? ")
+    phone_number = input("\nwhat is your phone number? ")
     file = open(os.path.join(dir, filename), 'w')
     file.write(f"{name}, {address}, {phone_number}")
     file.close()
     
 def read_in_file(dir, filename):
+    # print(dir)
+    # print(filename)
     file = open(os.path.join(dir, filename), 'r')
+    # print(file)
     # this will only give read access
+    print("\n\nContents: \n")
     for line in file:
-        print(line)
+        print(f"{line} \n")
     file.close()
     
 def directory_exist(dir):
@@ -41,9 +45,4 @@ def directory_exist(dir):
         print("Folder Created")
         return directory
         
-    
-
-    
-    
-    
 main()
